cd3702cfe4767061c252cc9e59d8e56a2ead3e4b
5cobak <55190870+5cobak@users.noreply.github.com>
2021-03-25 22:02:56 +0400

    added reference to gh-pages into README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # space-x
+
+[gh-pages](https://5cobak.github.io/space-x/)
--- webpack.config.js
+++ webpack.config.js
@@ -81,6 +81,7 @@ module.exports = {
     filename: filename('js'),
     path: path.resolve(__dirname, 'dist'),
   },
+  publicPath: '/space-x/',
   resolve: {
     extensions: ['.js'],
     alias: {
